78912041a6026ad3dd2f77ea1053a07f56f82215
Forpgm <phm.giamy@gmail.com>
2025-06-19 16:43:06 +0700

    platform revenue

--- fe/src/apis/admin.apis.js
+++ fe/src/apis/admin.apis.js
@@ -7,5 +7,7 @@ const adminApis = {
     http.get(`${adminUrl}/artists/applications/${status}`),
   verifyArtistApplication: (application_id, payload) =>
     http.post(`/${adminUrl}/artists/${application_id}/verify`, payload),
+  getPlatformRevenue: () => http.get(`${adminUrl}/balance`),
+  getAllAppointments: () => http.get(`${adminUrl}/appointments`),
 };
 export default adminApis;
