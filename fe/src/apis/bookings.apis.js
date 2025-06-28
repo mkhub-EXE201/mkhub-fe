a99223e82dc8d8de6aa981002277c8e55bb1c6bd
Forpgm <phm.giamy@gmail.com>
2025-06-28 21:32:58 +0700

    cancel bookings tab

--- fe/src/apis/bookings.apis.js
+++ fe/src/apis/bookings.apis.js
@@ -4,7 +4,10 @@ export const bookingUrl = "bookings";
 
 const bookingApis = {
   addBookingRequest: (payload) => http.post(`${bookingUrl}/add`, payload),
-  getBookingRequests: (type) => http.get(`${bookingUrl}?type=${type}`),
+  getBookingRequests: (type) => {
+    const url = type ? `${bookingUrl}?type=${type}` : bookingUrl;
+    return http.get(url);
+  },
   verifyBookingRequest: (booking_id, payload) =>
     http.post(`${bookingUrl}/verify/${booking_id}`, payload),
 };
